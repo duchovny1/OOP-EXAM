22f33e6e296096dc0e6ce8bfd5094027a961ddf7
rptlonfire <aduhowni123@gmail.com>
2019-11-07 20:03:15 +0000

    aadsas

---
+++ Polymorphism/Vehicles/IEmptyBus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+    public interface IEmptyBus
+    {
+        void IsTheBusEmpty(bool isEmpty);
+    }
+}
