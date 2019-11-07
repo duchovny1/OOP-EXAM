1b9bbd753a2fee80f7076a2957865753bc48d636
rptlonfire <aduhowni123@gmail.com>
2019-11-07 20:00:45 +0000

    asdas

---
+++ Polymorphism/Polymorphism/IOperation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operations
+{
+    public interface IOperation
+    {
+        int Add(int num1, int num2);
+        double Add(double num1, double num2, double num3);
+        decimal Add(decimal num1, decimal num2, decimal num3);
+
+    }
+}
---
+++ Polymorphism/Polymorphism/MathOperations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operations
+{
+    public class MathOperations : IOperation
+    {
+        public int Add(int num1, int num2)
+        {
+            int num = num1 + num2;
+            return num;
+        }
+
+        public double Add(double num1, double num2, double num3)
+        {
+            double num =
+                num1 + num2 + num3;
+            return num;
+        }
+
+        public decimal Add(decimal num1, decimal num2, decimal num3)
+        {
+            decimal num =
+                num1 + num2 + num3;
+            return num;
+        }
+    }
+}
