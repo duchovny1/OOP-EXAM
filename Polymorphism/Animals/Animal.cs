88c1e5a57c429cadeac41f97784c849961c46f65
rptlonfire <aduhowni123@gmail.com>
2019-11-07 19:58:36 +0000

    Polymorphism Applied

---
+++ Polymorphism/Animals/Animal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        private string name;
+        private string favouriteFood;
+
+        public Animal(string name, string food)
+        {
+            this.Name = name;
+            this.FavouriteFood = food;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            protected set => this.name = value;
+        }
+
+        public string FavouriteFood
+        {
+            get => this.favouriteFood;
+            protected set => this.favouriteFood = value;
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my favourite food is {this.FavouriteFood}";
+        }
+    }
+}
---
+++ Polymorphism/Animals/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace Animals
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Pesho", "Whiskas");
+            Dog dog = new Dog("Gosho", "Meat");
+
+            System.Console.WriteLine(cat.ExplainSelf());
+            System.Console.WriteLine(dog.ExplainSelf());
+
+        }
+    }
+}
