5614b9291452b62633dbc0a615d130c9da973898
rptlonfire <aduhowni123@gmail.com>
2019-11-10 17:26:13 +0000

    Inheritance exercise

---
+++ Inheritance/NeedForSpeed/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Motorcycle motorcycle = new Motorcycle(200, 10);
+            motorcycle.Drive(10);
+            System.Console.WriteLine(motorcycle.Fuel);
+            System.Console.WriteLine(motorcycle.FuelConsumption);
+
+
+            RaceMotorcycle racer = new RaceMotorcycle(200, 100);
+            racer.Drive(10);
+            System.Console.WriteLine(racer.Fuel);
+            System.Console.WriteLine(racer.FuelConsumption);
+            System.Console.WriteLine(motorcycle.FuelConsumption);
+        }
+    }
+}
---
+++ Inheritance/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,33 @@
+﻿namespace NeedForSpeed
+{
+    using System;
+
+    public class Vehicle
+    {
+        private const double DefaultFuelConsumption = 1.25;
+
+        public Vehicle(int horsePower, double fuel)
+        {
+            this.HorsePower = horsePower;
+            this.Fuel = fuel;
+        }
+        
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+
+        public double Fuel { get; set; }
+
+        public int HorsePower { get; set; }
+
+        public virtual void Drive(double kilometers)
+        {
+
+            double consumptedFuel = kilometers * this.FuelConsumption;
+
+            if (Fuel >= consumptedFuel)
+            {
+                this.Fuel -= consumptedFuel;
+            }
+        }
+
+    }
+}
