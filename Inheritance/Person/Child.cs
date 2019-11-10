282b95dafba4d0fb1de7cef925ff0aa49c681713
rptlonfire <aduhowni123@gmail.com>
2019-11-10 17:27:47 +0000

    Inheritance exercise

---
+++ Inheritance/Person/Child.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Child : Person
+    {
+        private int age;
+
+        public override int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (this.Age < 15 && this.Age > -1)
+                {
+                    this.age = Age;
+                }
+            }
+        }
+
+
+        public Child(string name, int age)
+            : base(name, age)
+        {
+             
+        }
+    }
+}
---
+++ Inheritance/Person/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name { get; set; }
+
+        public virtual int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            set
+            {
+                if (this.age >= 0 )
+                {
+                    this.age = Age;
+                }
+            }
+        }
+
+
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.Age = age;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(String.Format("Name: {0}, Age: {1}", this.Name, this.age));
+
+            return sb.ToString();
+        }
+
+    }
+}
