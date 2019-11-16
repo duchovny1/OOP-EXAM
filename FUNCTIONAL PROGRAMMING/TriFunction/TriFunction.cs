3b38805f09008aba969b282cb3d1fde1e043ab2f
rptlonfire <aduhowni123@gmail.com>
2019-11-16 09:38:42 +0000

    Create TriFunction.cs

---
+++ FUNCTIONAL PROGRAMMING/TriFunction/TriFunction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace TriFunction
+{
+    class TriFunction
+    {
+        static void Main(string[] args)
+        {
+            int givenNumbers = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            Func<string, int, bool> largerNumbers = (x, y) => x.Sum(b => b) >= y;
+
+            names = names.Where(name => largerNumbers(name, givenNumbers)).ToList();
+
+            if (names.Count != 0)
+            {
+                Console.WriteLine(names[0]);
+            }
+
+        }
+    }
+}
