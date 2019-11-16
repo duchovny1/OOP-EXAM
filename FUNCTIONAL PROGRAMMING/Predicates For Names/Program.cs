cfcb83497886714f7c837702d2dd782df4b18302
rptlonfire <aduhowni123@gmail.com>
2019-11-16 09:38:16 +0000

    Create Program.cs

---
+++ FUNCTIONAL PROGRAMMING/Predicates For Names/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Predicates_For_Names
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine()
+                .Split().ToList();
+
+            Predicate<string> eachName = x => x.Length <= number;
+
+            names = names.Where(name => eachName(name)).ToList();
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
